b41d6a771e0499299f3368758c8b4696fba407cb
Dustin <dustin.grady@gmail.com>
2018-04-07 00:47:19 -0700

    Testing quotes url call

--- src/main/java/com/example/microservicesdgrady/quotes/web/QuotesWeb.java
+++ src/main/java/com/example/microservicesdgrady/quotes/web/QuotesWeb.java
@@ -1,7 +1,8 @@
 package com.example.microservicesdgrady.quotes.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +12,18 @@ import com.example.microservicesdgrady.quotes.logic.QuotesLogicManager;
 
 @RestController
 public class QuotesWeb {
-	@Value("${quotesApiUrl:https://talaikis.com/api/quotes/random/}")
-	private String quotesApiUrl;
+	//@Value("${quotesApiUrl:https://talaikis.com/api/quotes/random/}")
+	//private String quotesApiUrl;
 	//@Value("${quotesApiKey:123456}")
 	//private String quotesApiKey;
 	@Autowired
 	QuotesLogicManager manager;
 	@RequestMapping("/quotes")
 	@ResponseBody
-	QuotesWebResult quotes(){
-		QuotesWebResult qwRes = new QuotesWebResult();
-		qwRes.setApiUrl(quotesApiUrl);
-		return qwRes;
+	ResponseEntity<String> quotes(){
+		ResponseEntity<String> result = manager.obtainQuoteApiImageUrl();
+		//QuotesWebResult qwRes = new QuotesWebResult();
+		//qwRes.setApiUrl(quotesApiUrl);
+		return result;
 	}
 }
